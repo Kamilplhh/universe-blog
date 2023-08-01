5525b1d18435b0ef9be383d24c0dbd5674ae621a
Kamil <kamilczekotas@gmail.com>
2023-08-01 18:17:32 +0200

    Css import from js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,13 +4,11 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/cs/app.css')
     @vite('resources/js/app.js')
 </head>
 
 <body>
     @section('background')
-
     @show
     <div class="content">
         @yield('content')
