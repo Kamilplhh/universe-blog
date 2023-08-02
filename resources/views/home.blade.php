7308846f18a810496ed569d7782eafda060c8768
Kamil <kamilczekotas@gmail.com>
2023-08-02 22:11:21 +0200

    Styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,26 @@
     <div class="homeContent">
         <h1>HOME</h1>
         <div class="row">
-            <div class="col-sm">
-                One
+            <div class="col-sm block">
+                <div class="photo"></div>
+                <div class="text">
+                    <p>MILKY WAY, OBSERVING</p>
+                    Find the center of the Milky Way: This Week in Astronomy with Dave Eicher
+                </div>
             </div>
-            <div class="col-sm">
-                Two
+            <div class="col-sm block">
+                <div class="photo"></div>
+                <div class="text">
+                    <p>EXOTIC OBJECTS, MILKY WAY</p>
+                    Rising star in astronomy: Katie Bouman
+                </div>
             </div>
-            <div class="col-sm">
-                Three
+            <div class="col-sm block">
+                <div class="photo"></div>
+                <div class="text">
+                    <p>COSMOLOGY, MILKY WAY, SCIENCE</p>
+                    The Great Hum: Scientists finally detect continuous gravitational waves rippling across space-time
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,8 @@
     <title>🌑🌒🌓🌔🌕</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Belanosima:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@700&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/js/app.js')
 </head>
