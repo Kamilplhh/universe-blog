08965f32d69479bea8ec4c3b4facca54faf876ff
Kamil <kamilczekotas@gmail.com>
2023-08-02 22:28:42 +0200

    Styling progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,6 +27,52 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-sm block">
+                <div class="photo"></div>
+                <div class="text">
+                    <p>MILKY WAY, OBSERVING</p>
+                    Find the center of the Milky Way: This Week in Astronomy with Dave Eicher
+                </div>
+            </div>
+            <div class="col-sm block">
+                <div class="photo"></div>
+                <div class="text">
+                    <p>EXOTIC OBJECTS, MILKY WAY</p>
+                    Rising star in astronomy: Katie Bouman
+                </div>
+            </div>
+            <div class="col-sm block">
+                <div class="photo"></div>
+                <div class="text">
+                    <p>COSMOLOGY, MILKY WAY, SCIENCE</p>
+                    The Great Hum: Scientists finally detect continuous gravitational waves rippling across space-time
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm block">
+                <div class="photo"></div>
+                <div class="text">
+                    <p>MILKY WAY, OBSERVING</p>
+                    Find the center of the Milky Way: This Week in Astronomy with Dave Eicher
+                </div>
+            </div>
+            <div class="col-sm block">
+                <div class="photo"></div>
+                <div class="text">
+                    <p>EXOTIC OBJECTS, MILKY WAY</p>
+                    Rising star in astronomy: Katie Bouman
+                </div>
+            </div>
+            <div class="col-sm block">
+                <div class="photo"></div>
+                <div class="text">
+                    <p>COSMOLOGY, MILKY WAY, SCIENCE</p>
+                    The Great Hum: Scientists finally detect continuous gravitational waves rippling across space-time
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @stop
