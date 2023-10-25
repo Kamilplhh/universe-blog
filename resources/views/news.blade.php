596aee9034589b9e400abc279ea10e0d2ee98e26
Kamil <kamilczekotas@gmail.com>
2023-10-25 18:48:20 +0200

    feat: news page

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.master')
+@vite('resources/css/home.css')
 
 @section('content')
+<div class="home">
+    <div class="homeContent">
+        <h1>News</h1>
+        <!-- Big block -->
+        <div class="row">
+            <div class="text-center col-12">
+                <p>MILKY WAY, OBSERVING</p>
+            </div>
+            <div class="col-xl-10 offset-xl-1 col-lg-12">
+                <div class="photo onePhoto"></div>
 
+            </div>
+            <div class="photo col-5 mt-5"></div>
+            <div class="text col-7 mt-5">
+                <p>EXOTIC OBJECTS, MILKY WAY</p>
+                Rising star in astronomy: Katie Bouman
+            </div>
+            <div class="col-12 mt-5">
+                <p>EXOTIC OBJECTS, MILKY WAY</p>
+                Rising star in astronomy: Katie Bouman
+            </div>
+        </div>
+
+    </div>
+</div>
 @stop
