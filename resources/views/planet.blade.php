0d22305619f595e71b5b897f4fb3e583a5ee200b
Kamil <kamilczekotas@gmail.com>
2023-10-25 19:22:44 +0200

    feat: planet page

--- resources/views/planet.blade.php
+++ resources/views/planet.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.master')
+@vite('resources/css/home.css')
 
 @section('content')
+<div class="home">
+    <div class="homeContent">
+        <h1>News</h1>
+        <!-- Big block -->
+        <div class="row">
+                <div class="col-12"></div>
+                <div class="text-center col-12 mt-5">
+                    <p>Saturn</p>
+                </div>
+                
+            </div>
+            <div class="col-12 mt-5">
+                Rising star in astronomy: Katie Bouman
+            </div>
+        </div>
 
+    </div>
+</div>
 @stop
