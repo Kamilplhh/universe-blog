f774250a7ea67ec3e752f10ee4bcf71d70d1a5b3
Kamil <kamilczekotas@gmail.com>
2023-10-27 23:40:54 +0200

    feat: styling for style 2

--- resources/js/add.js
+++ resources/js/add.js
@@ -2,18 +2,26 @@ import '../css/add.css';
 import $ from 'jquery';
 window.$ = $;
 
-const firstS = '<div class="photo addPhoto col-6 offset-md-3 mt-1 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>'+
-    '<input type="text" class="col-6 offset-md-3 mt-1 mb-4 addInput" placeholder="News title"></input>'+
-'<textarea class="col-7 mt-2 addInput" placeholder="News main text"></textarea>'+
-'<div class="photo addPhoto offset-md-1 col-4 my-5 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>'+
-'<div class="photo addPhoto col-4 my-5 mb-4 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>'+
-'<textarea class="col-7 offset-md-1 mt-4 addInput" placeholder="Second main text"></textarea>';
+const firstS = '<div class="photo addPhoto col-6 offset-md-3 mt-1 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>' +
+    '<input type="text" class="col-6 offset-md-3 mt-1 mb-4 addInput" placeholder="News title"></input>' +
+    '<textarea class="col-7 mt-2 addInput" placeholder="News main text"></textarea>' +
+    '<div class="photo addPhoto offset-md-1 col-4 my-5 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>' +
+    '<div class="photo addPhoto col-4 my-5 mb-4 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>' +
+    '<textarea class="col-7 offset-md-1 mt-4 addInput" placeholder="Second main text"></textarea>';
 
-const secondS = "<p>2</p>";
+const secondS = '<input type="text" class="col-6 offset-md-3 mt-1 mb-4 addInput" placeholder="News title"></input>' +
+    '<div class="photo addPhoto col-6 offset-md-3 mt-1 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>' +
+    '<div></div>' +
+    '<div class="photo addPhoto col-4 mt-5 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>' +
+    '<div class="photo addPhoto px-2 col-4 mt-5 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>' +
+    '<div class="photo addPhoto col-4 mt-5 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>' +
+    '<textarea class="col-12 my-4 addInput" placeholder="News main text"></textarea>' +
+    '<div class="photo addPhoto col-4 my-5 mb-4 d-flex flex-column justify-content-center align-items-center">ADD PHOTO</div>' +
+    '<textarea class="col-7 offset-md-1 mt-4 addInput" placeholder="Second main text"></textarea>';
 
 const thirdS = "<p>3</p>";
 
-$( document ).ready(style1());
+$(document).ready(style2());
 
 $("#style-select").on("change", function () {
     let selected = $("#style-select option:selected").val();
