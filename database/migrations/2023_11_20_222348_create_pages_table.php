6c3597e94544ddaf603f18f9028b9fd581cdb2c5
Kamil <kamilczekotas@gmail.com>
2023-11-20 23:35:17 +0100

    feat: database set, page model and migration data

---
+++ database/migrations/2023_11_20_222348_create_pages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('mainText');
+            $table->text('secondText')->nullable();
+            $table->string('photo1');
+            $table->string('photo2')->nullable();
+            $table->string('photo3')->nullable();
+            $table->string('photo4')->nullable();
+            $table->string('photo5')->nullable();
+            $table->bigInteger('format');
+            $table->string('user')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pages');
+    }
+};
