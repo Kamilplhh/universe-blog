abf0aae5fbd5e3f2f4ce19d8df94cbe7909bd46b
Kamil <kamilczekotas@gmail.com>
2023-11-22 23:39:32 +0100

    feat: swap page model for information

--- database/migrations/2023_11_22_223848_create_information_table.php
+++ database/migrations/2023_11_22_223848_create_information_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('pages', function (Blueprint $table) {
+        Schema::create('information', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('mainText');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('pages');
+        Schema::dropIfExists('information');
     }
 };
