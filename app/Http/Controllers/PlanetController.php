b30694eb8d5b104c1daec0de477b7c14cfd48f4e
Kamil <kamilczekotas@gmail.com>
2023-11-29 23:44:50 +0100

    feat: planet controller data passing system

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Planet;
 
 class PlanetController extends Controller
 {
-    //
+    public function getAll() 
+    {
+        $objects = Planet::inRandomOrder()
+        ->where('seen', '=', 'no')
+        ->limit(1)
+        ->get();
+        
+        return view('planet', compact('objects'));
+    }
 }
