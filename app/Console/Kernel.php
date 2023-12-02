80b84ed25140eab0c6b27f73f1f58fd17c1790b3
Kamil <kamilczekotas@gmail.com>
2023-12-02 23:16:21 +0100

    feat: command scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('set:planet')->dailyAt('00:00');
+        $schedule->command('reset:planets')->yearly();
     }
 
     /**
