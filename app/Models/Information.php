dcda5b6edef26341887897cec6e6b13b624e3e1a
Kamil <kamilczekotas@gmail.com>
2024-01-01 20:26:34 +0100

    feat: models fillable

--- app/Models/Information.php
+++ app/Models/Information.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Information extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'mainText',
+        'secondText',
+        'photo1',
+        'photo2',
+        'photo3',
+        'photo4',
+        'photo5',
+        'format',
+        'user',
+        'category',
+    ];
 }
