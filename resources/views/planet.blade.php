6157c88aedefa30aca48f0be5443ec4b1847280d
Kamil <kamilczekotas@gmail.com>
2024-01-07 21:17:55 +0100

    fix: data print on planet.blade

--- resources/views/planet.blade.php
+++ resources/views/planet.blade.php
@@ -6,21 +6,23 @@
     <div class="homeContent">
         <h1>Today's planet</h1>
         <div class="row">
+            @foreach($object as $object)
                 @if(isset($object['photo']))
                 <div class="col-12">
                     {{ $object['photo'] }}
                 </div>
                 @endif
                 <div class="text-center col-12 mt-5">
-                    <p>{{ $object['title'] }}</p>
+                    <h2><p>{{ $object['title'] }}</p></h2>
                 </div>
                 
             </div>
             <div class="col-12 mt-5">
                 {{ $object['text'] }}
             </div>
+            @endforeach
         </div>
-
+        
     </div>
 </div>
 @stop
